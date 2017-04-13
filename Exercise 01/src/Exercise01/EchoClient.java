80e66fa20c29343732781b5219ebd977159afa47
Jose Juan Sandoval <juanchis_sandoval@hotmail.com>
2017-04-13 13:04:58 -0700

    Implemented Multithreading and Networking Code
    -Finished implementing the networking code to enable communication
    between client and server.
    -Implemented multithreading for the server to be able to handle multiple
    simoultaneous client connections.

--- Exercise 01/src/Exercise01/EchoClient.java
+++ Exercise 01/src/Exercise01/EchoClient.java
@@ -2,50 +2,56 @@ package Exercise01;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public final class EchoClient
 {
-
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args) throws IOException
     {
+        String hostName = "localhost";
+        int portNumber = 22222;
 
-
-        try (Socket socket = new Socket("localhost", 22222))
+        try (Socket socket = new Socket(hostName, portNumber))
         {
-            //String message;
-            //Scanner scanner = new Scanner(System.in);
+            // Objects needed for receiving and reading the server's messages.
+            String message;
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader in = new BufferedReader(inputStreamReader);
+
+            // Objects needed for sending messages to the server.
+            Scanner scanner = new Scanner(System.in);
             OutputStream outputStream = socket.getOutputStream();
             PrintStream out = new PrintStream(outputStream, false, "UTF-8");
-            out.printf("Hello from ");
 
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
+            // The main loop of execution.
+            // It only executes when the server has sent a message.
+            while((message = in.readLine()) != null)
+            {
+                System.out.println("Server> " + message);
+                System.out.print("Client> ");
+                message = scanner.nextLine();
 
+                // Sends the message to the server.
+                out.println(message);
 
-            //System.out.println("Connected to: " + socket.getRemoteSocketAddress());
-
-
-
-//            do
-//            {
-//                System.out.print("Client> ");
-//                message = scanner.nextLine();
-//
-//
-//
-//
-//
-//
-//            }while(!message.toUpperCase().equals("EXIT"));
-
-            //socket.notify();
+                if(message.toUpperCase().equals("EXIT"))
+                    break;
+            }
+            socket.close();
         }
-        catch (Exception exception)
+        catch (UnknownHostException e)
         {
-            System.out.println(exception.getMessage());
+            System.err.println("ERROR: Unknown host " + hostName + ".");
+        }
+        catch (Exception e)
+        {
+            System.err.println("ERROR: Could not connect to " + hostName + ".");
+        }
+        finally
+        {
+            System.exit(1);
         }
     }
 }
